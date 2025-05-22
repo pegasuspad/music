e1b9340dc27ff08761dc83c51b96bc1c2c5a0fdc
skleinjung <sean@thrashplay.com>
2025-05-22 00:04:40 -0500

    update entrypoint with simple demo of new Frame rendering

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,126 @@
 import { NovationLaunchpadMiniMk3 } from './vendors/novation/launchpad-mini-mk3/novation-launchpad-mini-mk3.ts'
+import { Frame } from './vendors/novation/launchpad-mini-mk3/ui/frame.ts'
+import type { PadLighting } from './vendors/novation/launchpad-mini-mk3/commands/set-led-lighting.ts'
+import type { LightingOptions } from './vendors/novation/launchpad-mini-mk3/model.ts'
+
+const drawButton = (
+  frame: Frame,
+  {
+    x,
+    y,
+    lighting,
+  }: {
+    x: number
+    y: number
+    lighting: LightingOptions
+  },
+) => {
+  frame.set(x, y, lighting)
+}
+
+const drawRectangle = (
+  frame: Frame,
+  {
+    x,
+    y,
+    width,
+    height,
+    lighting,
+  }: {
+    x: number
+    y: number
+    width: number
+    height: number
+    lighting: LightingOptions
+  },
+) => {
+  for (let currentX = x; currentX < x + width; currentX++) {
+    for (let currentY = y; currentY < y + height; currentY++) {
+      frame.set(currentX, currentY, lighting)
+    }
+  }
+}
 
 const main = (): Promise<void> => {
   return new Promise(() => {
-    new NovationLaunchpadMiniMk3()
+    const launchpad = new NovationLaunchpadMiniMk3()
+    const frame = new Frame()
+    let x = 0
+
+    const update = () => {
+      x = (x + 1) % 9
+    }
+
+    const render = () => {
+      drawRectangle(frame, {
+        x: x - 1,
+        y: 2,
+        width: 3,
+        height: 4,
+        lighting: {
+          type: 'pulsing',
+          color: 53,
+        },
+      })
+
+      drawRectangle(frame, {
+        x,
+        y: 0,
+        width: 1,
+        height: 8,
+        lighting: {
+          type: 'static',
+          color: 33,
+        },
+      })
+
+      drawButton(frame, {
+        x,
+        y: 0,
+        lighting: {
+          type: 'static',
+          color: 45,
+        },
+      })
+      drawButton(frame, {
+        x,
+        y: 7,
+        lighting: {
+          type: 'static',
+          color: 45,
+        },
+      })
+    }
+
+    const showFrame = () => {
+      frame.render((values) => {
+        const pads = values.map((value) =>
+          value.lighting === null ?
+            {
+              ...value,
+              lighting: {
+                color: 0,
+                type: 'static',
+              },
+            }
+          : value,
+        )
+
+        void launchpad.sendCommand('set-led-lighting', {
+          pads: pads as PadLighting[],
+        })
+      })
+    }
+
+    const loop = () => {
+      update()
+      render()
+      showFrame()
+
+      setTimeout(loop, 500)
+    }
+
+    loop()
   })
 }
 
