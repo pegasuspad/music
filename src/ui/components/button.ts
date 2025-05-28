65b963e7fd496d268c65f858cdfb7314be0938a4
skleinjung <sean@thrashplay.com>
2025-05-27 20:34:28 -0500

    update button to allow configurable event handling

--- src/ui/components/button.ts
+++ src/ui/components/button.ts
@@ -1,19 +1,23 @@
-import { logger } from '../../logger.ts'
 import type { RgbColor } from '../color.ts'
 import type { Drawable } from '../drawable.ts'
+import type { PressEvent } from '../input/input-event.ts'
 
 /**
  * Creates a `Drawable` which displays a single-cell button with the specified color.
  */
-export const createButton = (color: RgbColor): Drawable<RgbColor> => ({
+export const createButton = ({
+  color = [127, 127, 127],
+  onPress,
+}: {
+  color?: RgbColor
+  onPress?: (event: PressEvent) => void
+}): Drawable<RgbColor> => ({
   draw: () => [
     {
       value: color,
       x: 0,
       y: 0,
-      onPress: (event) => {
-        logger.info({ event }, 'got but event')
-      },
+      onPress,
     },
   ],
 })
