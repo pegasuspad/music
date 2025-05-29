c897c03e8fad859b0d9ed4d73c8be27fbe3bda4a
skleinjung <sean@thrashplay.com>
2025-05-29 12:12:01 -0500

    add parsers for SysEx messages, both universal and launchpad-specific

---
+++ src/vendors/novation/launchpad-mini-mk3/sysex-messages.ts
@@ -0,0 +1,38 @@
+import type { UnknownMessage } from '../../../midi/sysex-message-parser.ts'
+import {
+  isNovationSysExMessage,
+  type BaseNovationMessage,
+} from '../novation-sysex.ts'
+
+export interface ReadbackMessage extends BaseNovationMessage {
+  /**
+   * Numeric ID of the command sending the readback.
+   */
+  command: number
+
+  /**
+   * Readback data.
+   */
+  payload: number[]
+
+  type: 'readback'
+}
+
+export type LaunchpadSysExMessage = ReadbackMessage
+
+export const parseSysexMessage = (
+  message: UnknownMessage,
+): LaunchpadSysExMessage | UnknownMessage => {
+  const isReadback = message.data[0] === 0x02 && message.data[1] === 0x0d
+
+  if (isNovationSysExMessage(message) && isReadback) {
+    return {
+      ...message,
+      command: message.data[2],
+      payload: message.data.slice(3),
+      type: 'readback',
+    }
+  } else {
+    return message
+  }
+}
