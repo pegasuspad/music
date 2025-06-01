dd8edf4a1274ea7a7256b127c89ea97494e3f81f
Sean Kleinjung <sean@thrashplay.com>
2025-06-01 18:30:55 +0000

    fix 'speak' function for both web and nodejs use

--- src/app/speak.ts
+++ src/app/speak.ts
@@ -1,10 +1,11 @@
-import { exec } from 'node:child_process'
 import { isBrowser } from './is-browser.ts'
 
 export const speak = (text: string) => {
   if (isBrowser()) {
     console.warn(`Unable to 'speak' text from browser: ${text}`)
   } else {
-    exec(`say ${JSON.stringify(text)}`)
+    void import('node:child_process').then(({ exec }) => {
+      exec(`say ${JSON.stringify(text)}`)
+    })
   }
 }
