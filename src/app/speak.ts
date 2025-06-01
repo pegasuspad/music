8e9c60e5dfba8ef594a900ba3bda9af4d0b02d36
skleinjung <sean@thrashplay.com>
2025-06-01 08:19:50 -0500

    fix bug preventing non-browser usage, but browser usage not functioning currently

--- src/app/speak.ts
+++ src/app/speak.ts
@@ -1,15 +1,10 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-
+import { exec } from 'node:child_process'
 import { isBrowser } from './is-browser.ts'
 
 export const speak = (text: string) => {
   if (isBrowser()) {
     console.warn(`Unable to 'speak' text from browser: ${text}`)
   } else {
-    const child_process = require('node:child_process')
-    child_process.exec(`say ${JSON.stringify(text)}`)
+    exec(`say ${JSON.stringify(text)}`)
   }
 }
